4cadbd411db685258af172f940b60a0f8b1e826b
Dragoş <edragos33@gmail.com>
2025-04-14 17:35:25 +0200

    sqlite integrated

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length = 15, unique = True, blank = False)
+    first_name = models.CharField(max_length= 15, blank = False)
+    last_name = models.CharField(max_length= 35, blank = False)
+    emails = models.EmailField(max_length=50,  blank=False, unique=True)
+    password = models.CharField(max_length=25, blank=False)
