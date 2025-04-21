39a448cdfbf42abd2f8084654d56f0c4ddba0751
Dragoş <edragos33@gmail.com>
2025-04-21 16:05:23 +0200

    sign up page is functional

---
+++ myapp/forms.py
@@ -0,0 +1,49 @@
+﻿from django import forms  
+from django.contrib.auth.models import User  
+from django.contrib.auth.forms import UserCreationForm  
+from django.core.exceptions import ValidationError  
+from django.forms.fields import EmailField  
+from django.forms.forms import Form  
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        label='username',
+        min_length=5,
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Username',
+            'class': '', 
+        })
+    )
+    email = forms.EmailField(
+        label='email',
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'E-mail address',
+            'class': '',
+        })
+    )
+    password1 = forms.CharField(
+        label='password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password',
+            'class': '',
+        })
+    )
+    password2 = forms.CharField(
+        label='Confirm password',
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Confirm password',
+            'class': '',
+        })
+    )
+    
+    def save(self, commit=True):
+        print("✅ Saving user:", self.cleaned_data['username'])
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        if commit:
+            user.save()
+        return user
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 def home(request):
     return render(request, 'myapp/home.html')
 
+# ---------- Log in page ----------
 def login(request):
     return render(request, 'myapp/login.html')
 
+# ---------- Sign Up page ---------- 
 def signup(request):
-    return render(request, 'myapp/signup.html')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("✅ User created")
+        else:
+            print("❌ Form is not valid")
+            print(form.errors)  # <-- This shows why it's failing
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'myapp/signup.html', {'form': form})
 
 def forgot_password(request):
     return render(request, 'myapp/forgotPassword.html')
+
