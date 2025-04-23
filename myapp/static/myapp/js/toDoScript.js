b7b4798dd23d970af24c9c8f9c72cf98e9735b54
Dragoş <edragos33@gmail.com>
2025-04-23 17:15:23 +0200

    clear text area after send

--- myapp/static/myapp/js/toDoScript.js
+++ myapp/static/myapp/js/toDoScript.js
@@ -1,21 +1,26 @@
 let submitCategory = document.querySelector(".btn");
 let addCategory = document.querySelector(".category-txt");
 let taskList = document.querySelector("#task-list");
+let miniBtn = document.querySelector(".miniBtn");
 
 function createNewCategory(taskTitle){
     // Create h2 title
     let newtitle = document.createElement("h2");
     newtitle.classList.add("category-title");
-    newtitle.innerText = taskTitle;
+    newtitle.innerText = taskTitle;// clean the text area after pressed send
 
     // Create textarea
     let textField = document.createElement("textarea");
     textField.classList.add("newField");
+    addCategory.value = "";
 
     // Create new button
     let secondaryBtn = document.createElement("button");
     secondaryBtn.innerText = "Add new task"
     secondaryBtn.classList.add("miniBtn");
+
+    let task = document.createElement("input");
+    task.type = 'checkbox';
 
     let categoryContainer = document.createElement("div");
     categoryContainer.classList.add("category-container");
@@ -23,10 +28,15 @@ function createNewCategory(taskTitle){
     categoryContainer.appendChild(textField); // append textField
     categoryContainer.appendChild(secondaryBtn); // append secondaryBtn
     taskList.appendChild(categoryContainer); // append categoryContainer
+
     
 }
 
 submitCategory.addEventListener("click", function(){
     let taskTitle = addCategory.value;
     createNewCategory(taskTitle);
-})
+});
+
+// miniBtn.addEventListener("click", function(){
+//     console.log("clicked")
+// })
